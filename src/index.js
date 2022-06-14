298d2778928c4b8553ce3ebd042c3bb679344c28
Patryk Tomasz Tomaszek <patryk.tomasz.tomaszek@gmail.com>
2022-06-14 21:18:46 +0200

    Another bugfix

--- src/index.js
+++ src/index.js
@@ -17,7 +17,6 @@ const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
 const info = document.querySelector('.country-info');
-const reset = documenmt.query
 
 let switchOnce = false; // Variable for disabling notification after first popup
 
