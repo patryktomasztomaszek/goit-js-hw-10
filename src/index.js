748db626c80a145da56f17d59cbe322294d0d095
Patryk Tomasz Tomaszek <patryk.tomasz.tomaszek@gmail.com>
2022-06-14 15:37:43 +0200

    Extra stuff
    Did soma (imho) amazing stuff!

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './css/styles.css';
 import { fetchCounrtries } from './js/fetchCountries.js';
 
 // Importing lodash - Debounce function
-import { debounce, values } from 'lodash';
+import { debounce } from 'lodash';
 
 // Importing Notiflix - Notify function
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
@@ -18,34 +18,33 @@ const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
 const info = document.querySelector('.country-info');
 
+let switchOnce = false; // Variable for disabling notification after first popup
+
 var listOfCountries = null; // Used in selecting country from list
 
 // Adding functionality for search result,
 // Clicking country will expand on page
 const selectCountry = event => {
   event.preventDefault();
-  console.log(event.target.nodeName);
 
   //  Checking for valid target
-  if ( event.target.nodeName === "UL" ) {
-    console.log("Invalid target");
+  if (event.target.nodeName === 'UL') {
     return;
   }
-console.log(event.target.textContent.trim());
-input.value = event.target.textContent.trim();
-handleInputValue("name", event.target.textContent.trim());
+  input.value = event.target.textContent.trim();
+  handleInputValue(event.target.textContent.trim());
+};
 
+// Function dor purging current displayed data
+const purgeDisplay = () => {
+  info.innerHTML = '';
+  list.innerHTML = '';
 };
 
 // Function for fetched data markup output
 function displayData(data) {
-  //   purging current displayed data
-  const purgeDisplay = () => {
-    list.innerHTML = '';
-    info.innerHTML = '';
-  };
-
   //   Selecting markup style depending on fetched output arrays count
+
   if (data.length > 10) {
     purgeDisplay();
 
@@ -53,25 +52,29 @@ function displayData(data) {
       'Too many matches found. Please enter a more specific name.'
     );
   } else if (data.length < 10 && data.length > 1) {
-    console.log(data);
+    purgeDisplay();
+
     const listMarkup = data
       .map(
         ({ name, flags }) =>
           `
              <li class="country-list__item" select-country>
-                <span class-"country-list__name">
-                  <img class="country-list__image" src="${flags.svg}" >${name.common}
-                </span>
+               <img class="country-list__image" alt="Flag of ${name.common}" src="${flags.svg}" >
+               <span class="country-list__name">${name.common}</span>
              </li>
           `
       )
       .join('');
 
     list.innerHTML = listMarkup;
+
+    if (!switchOnce) {
+      switchOnce = true;
+      Notify.info('You can click on listed country, to show more!');
+    }
+
     listOfCountries = document.querySelectorAll('[select-country]');
-    console.log(listOfCountries);
     for (const country of listOfCountries) {
-      console.log(country);
       country.addEventListener('click', selectCountry);
     }
   } else if ((data.length = 1)) {
@@ -82,33 +85,43 @@ function displayData(data) {
         ({ name, capital, population, flags, languages }) =>
           `
         <h2 class="country-info__name">${name.official}</h2>
-        <img class="country-info__image" src="${
-          flags.svg
-        }" alt="Flag of ${name}" >
-        <p class="country-info__capital">Capital: ${capital}</p>
-        <p class="country-info__population>Population: ${population}</p>
-        <p class="country-info__languages>Languages: ${Object.values(
-          languages
-        ).join(', ')}</p>
+        <img class="country-info__image" src="${flags.svg}" alt="Flag of ${
+            name.common
+          }" >
+        <p class="country-info__capital">
+          <span class="country-info__label">Capital:</span>
+        ${capital}
+        </p>
+        <p class="country-info__population">
+          <span class="country-info__label">Population:</span>
+        ${population}
+        </p>
+        <p class="country-info__languages">
+          <span class="country-info__label">Languages:</span>
+          ${Object.values(languages).join(', ')}
+        </p>
 
         `
       )
       .join('');
-    list.innerHTML = infoMarkup;
+
+    info.innerHTML = infoMarkup;
   }
 }
 
 // Function for handling input value,
 // and initializing fetch and markup functions
 function handleInputValue() {
-  console.log(input.value);
-  fetchCounrtries('name', input.value.trim()).then(fetchedOutput => {
-    console.log(fetchedOutput);
-    displayData(fetchedOutput);
-  });
+  fetchCounrtries(input.value.trim())
+    .then(fetchedOutput => {
+      displayData(fetchedOutput);
+    })
+
+    .catch(error => {
+      purgeDisplay();
+      Notify.failure('No results found!');
+    });
 }
 
 // Initializing input listener for handler function
 input.addEventListener('input', debounce(handleInputValue, DEBOUNCE_DELAY));
-
-fetch('https://restcountries.com/v3.1/all');
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,5 @@
-export function fetchCounrtries( filter, input ) {
-  return fetch(`https://restcountries.com/v3.1/${filter}/${input}?fields=name,capital,population,flags,languages`)
+export function fetchCounrtries( input ) {
+  return fetch(`https://restcountries.com/v3.1/name/${input}?fields=name,capital,population,flags,languages`)
     .then(response => {
       if (response.ok) {
         return response.json();
@@ -8,6 +8,7 @@ export function fetchCounrtries( filter, input ) {
       }
     })
     .catch(error => {
+      purgeDisplay();
       console.log(`Encountered an ${error.message} error!`);
     });
 }
